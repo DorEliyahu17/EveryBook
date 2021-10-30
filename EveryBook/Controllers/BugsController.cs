3a40925ec757f8f25869139dd46a2827d335143d
Dor Eliyahu <doreliyahu17@gmail.com>
2021-10-30 19:13:54 +0300

    added "Bugs" controller

---
+++ EveryBook/Controllers/BugsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EveryBook.Data;
+using EveryBook.Models;
+
+namespace EveryBook.Controllers
+{
+    public class BugsController : Controller
+    {
+        private readonly EveryBookContext _context;
+
+        public BugsController(EveryBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bugs
+        public async Task<IActionResult> Index()
+        {
+            var everyBookContext = _context.Bug.Include(b => b.User);
+            return View(await everyBookContext.ToListAsync());
+        }
+
+        // GET: Bugs/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bug
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+
+            return View(bug);
+        }
+
+        // GET: Bugs/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Bugs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,UserId")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bug);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", bug.UserId);
+            return View(bug);
+        }
+
+        // GET: Bugs/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bug.FindAsync(id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", bug.UserId);
+            return View(bug);
+        }
+
+        // POST: Bugs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Title,Description,UserId")] Bug bug)
+        {
+            if (id != bug.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bug);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BugExists(bug.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", bug.UserId);
+            return View(bug);
+        }
+
+        // GET: Bugs/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bug = await _context.Bug
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bug == null)
+            {
+                return NotFound();
+            }
+
+            return View(bug);
+        }
+
+        // POST: Bugs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var bug = await _context.Bug.FindAsync(id);
+            _context.Bug.Remove(bug);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BugExists(long id)
+        {
+            return _context.Bug.Any(e => e.Id == id);
+        }
+    }
+}
