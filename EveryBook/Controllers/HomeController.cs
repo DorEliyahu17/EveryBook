e16133e839aff2c669853015fd2df2dc6851e0c5
Dor Eliyahu <doreliyahu17@gmail.com>
2021-11-05 01:11:21 +0200

    added is admin to database

--- EveryBook/Controllers/HomeController.cs
+++ EveryBook/Controllers/HomeController.cs
@@ -31,17 +31,7 @@ namespace EveryBook.Controllers
         public IActionResult About()
         {
             IEnumerable<DistributionUnit> everyBookContext = _context.DistributionUnit.Include(s => s.Location);
-            /*
-            List<Store> model = new List<Store>();
-            var locations = new List<Location>()
-            {
-                new Locations(1, "Ilan Malisov - ilan07m@gmail.com","Herzliya, israel", 32.163510, 34.842670),
-                new Locations(2, "Michael Tsiriulinikov - misha1235000@gmail.com","Bat Yam, israel", 32.011850, 34.745310),
-                new Locations(3, "Ofek Reuveni - ofekrv@gmail.com","Rishon Lezion, israel", 31.963760, 34.815720),
-                new Locations(3, "Colman Collage of management - 03-963-4000","Rishon Lezion, israel", 31.986460, 34.806020)
-            };
-            model.LocationList = locations;
-            */
+
             return View(everyBookContext);
         }
 
